7942db9650a9ca569bd44f7f4e418ca53c5ecd83
juanmurillog <jjmurillob@ut.edu.co>
2020-08-17 15:32:11 -0500

    subido el build

---
+++ build/precache-manifest.2ddf614420254a6e67fc9be6e59f48a7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f42841071d1551ca8a2939d326851a9f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fe7132a9d7182ede2a21",
+    "url": "/static/css/main.795f4b61.chunk.css"
+  },
+  {
+    "revision": "880bf4bd00c3a692ea0f",
+    "url": "/static/js/2.30b7af4f.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.30b7af4f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fe7132a9d7182ede2a21",
+    "url": "/static/js/main.a17f57e7.chunk.js"
+  },
+  {
+    "revision": "68fd825a81ed7bcabafa",
+    "url": "/static/js/runtime-main.cd99ccd7.js"
+  }
+]);
